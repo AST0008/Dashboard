1a5ad3f511adc293e1ac8694a0ffe44d718bfa9b
AST <ashwajittayade30@gmail.com>
2024-04-29 14:27:43 +0530

    Added Calender

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var myNodelist = document.getElementsByTagName("LI");
+var myNodelist = document.getElementById("myUL").getElementsByTagName("LI"); // Target only the to-do list items
 var i;
 for (i = 0; i < myNodelist.length; i++) {
   var span = document.createElement("SPAN");
@@ -11,25 +11,29 @@ for (i = 0; i < myNodelist.length; i++) {
 var close = document.getElementsByClassName("close");
 var i;
 for (i = 0; i < close.length; i++) {
-  close[i].onclick = function() {
+  close[i].onclick = function () {
     var div = this.parentElement;
     div.style.display = "none";
-  }
+  };
 }
 
-var list = document.querySelector('ul');
-list.addEventListener('click', function(ev) {
-  if (ev.target.tagName === 'LI') {
-    ev.target.classList.toggle('checked');
-  }
-}, false);
+var list = document.getElementById("myUL"); // Target only the to-do list
+list.addEventListener(
+  "click",
+  function (ev) {
+    if (ev.target.tagName === "LI") {
+      ev.target.classList.toggle("checked");
+    }
+  },
+  false
+);
 
 function newElement() {
   var li = document.createElement("li");
   var inputValue = document.getElementById("myInput").value;
   var t = document.createTextNode(inputValue);
   li.appendChild(t);
-  if (inputValue === '') {
+  if (inputValue === "") {
     alert("You must write something!");
   } else {
     document.getElementById("myUL").appendChild(li);
@@ -43,9 +47,116 @@ function newElement() {
   li.appendChild(span);
 
   for (i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
+    close[i].onclick = function () {
       var div = this.parentElement;
       div.style.display = "none";
-    }
+    };
   }
 }
+
+
+let date = new Date();
+let year = date.getFullYear();
+let month = date.getMonth();
+
+const day = document.querySelector(".calendar-dates");
+
+const currdate = document.querySelector(".calendar-current-date");
+
+const prenexIcons = document.querySelectorAll(".calendar-navigation span");
+
+// Array of month names
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+// Function to generate the calendar
+const manipulate = () => {
+  // Get the first day of the month
+  let dayone = new Date(year, month, 1).getDay();
+
+  // Get the last date of the month
+  let lastdate = new Date(year, month + 1, 0).getDate();
+
+  // Get the day of the last date of the month
+  let dayend = new Date(year, month, lastdate).getDay();
+
+  // Get the last date of the previous month
+  let monthlastdate = new Date(year, month, 0).getDate();
+
+  // Variable to store the generated calendar HTML
+  let lit = "";
+
+  // Loop to add the last dates of the previous month
+  for (let i = dayone; i > 0; i--) {
+    lit += `<li class="inactive">${monthlastdate - i + 1}</li>`;
+  }
+
+  // Loop to add the dates of the current month
+  for (let i = 1; i <= lastdate; i++) {
+    // Check if the current date is today
+    let isToday =
+      i === date.getDate() &&
+      month === new Date().getMonth() &&
+      year === new Date().getFullYear()
+        ? "active"
+        : "";
+    lit += `<li class="${isToday}">${i}</li>`;
+  }
+
+  // Loop to add the first dates of the next month
+  for (let i = dayend; i < 6; i++) {
+    lit += `<li class="inactive">${i - dayend + 1}</li>`;
+  }
+
+  // Update the text of the current date element
+  // with the formatted current month and year
+  currdate.innerText = `${months[month]} ${year}`;
+
+  // update the HTML of the dates element
+  // with the generated calendar
+  day.innerHTML = lit;
+};
+
+manipulate();
+
+// Attach a click event listener to each icon
+prenexIcons.forEach((icon) => {
+  // When an icon is clicked
+  icon.addEventListener("click", () => {
+    // Check if the icon is "calendar-prev"
+    // or "calendar-next"
+    month = icon.id === "calendar-prev" ? month - 1 : month + 1;
+
+    // Check if the month is out of range
+    if (month < 0 || month > 11) {
+      // Set the date to the first day of the
+      // month with the new year
+      date = new Date(year, month, new Date().getDate());
+
+      // Set the year to the new year
+      year = date.getFullYear();
+
+      // Set the month to the new month
+      month = date.getMonth();
+    } else {
+      // Set the date to the current date
+      date = new Date();
+    }
+
+    // Call the manipulate function to
+    // update the calendar display
+    manipulate();
+  });
+});
